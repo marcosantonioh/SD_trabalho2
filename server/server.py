b5856ec63c4813668f04baad9e3bc91f2eac9af8
Marcos Antônio <onetestt6@gmail.com>
2026-03-29 20:21:59 -0300

    adição do log txt

--- server/server.py
+++ server/server.py
@@ -1,15 +1,25 @@
 from flask import Flask, request, jsonify
+import datetime
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/dados', methods=['POST'])
 def receber_dados():
     data = request.json
-    print("\nDados recebidos:")
-    print(f"Bateria: {data.get('bateria')}%")
-    print(f"Memória: {data.get('memoria')}")
+    
+    bateria = data.get('bateria')
+    memoria = data.get('memoria')
+    
+    timestamp = datetime.datetime.now()
+
+    log = f"{timestamp} | Bateria: {bateria}% | Memória: {memoria}\n"
+
+    print(log)
+
+    with open("log.txt", "a") as arquivo:
+        arquivo.write(log)
+
     return jsonify({"status": "ok"})
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
